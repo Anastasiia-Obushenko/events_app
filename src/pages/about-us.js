5d1e664ed2bf26a35bff1fe24ac4a3702547049b
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-30 15:31:20 -0500

    about-us page content

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div>
+      <h1>AboutUs</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beatus sibi videtur esse moriens. Sine ea igitur iucunde negat posse se vivere? Sed est forma eius disciplinae, sicut fere ceterarum, triplex: una pars est naturae, disserendi altera, vivendi tertia. Quae quidem vel cum periculo est quaerenda vobis; Nec tamen exprimi verbum e verbo necesse erit, ut interpretes indiserti solent, cum sit verbum, quod idem declaret, magis usitatum. At vero illa, quae Peripatetici, quae Stoici dicunt, semper tibi in ore sunt in iudiciis, in senatu. Duo Reges: constructio interrete. Aliis esse maiora, illud dubium, ad id, quod summum bonum dicitis, ecquaenam possit fieri accessio. Si est nihil in eo, quod perficiendum est, praeter motum ingenii quendam, id est rationem, necesse est huic ultimum esse virtute agere; Dicuntur ista, Cato, magnifice, inquam, sed videsne verborum gloriam tibi cum Pyrrhone et cum Aristone, qui omnia exaequant, esse communem? Sin kakan malitiam dixisses, ad aliud nos unum certum vitium consuetudo Latina traduceret. Quantam rem agas, ut Circeis qui habitet totum hunc mundum suum municipium esse existimet? Quorum fuit haec institutio, in qua animadvertas velim quid mutandum putes nec expectes, dum ad omnia dicam, quae a te dicta sunt; Quam similitudinem videmus in bestiis, quae primo, in quo loco natae sunt, ex eo se non commoventi deinde suo quaeque appetitu movetur.
 
-export default AboutUs
+        Quia, cum a Zenone, inquam, hoc magnifice tamquam ex oraculo editur: Virtus ad beate vivendum se ipsa contenta est, et Quare? Restincta enim sitis stabilitatem voluptatis habet, inquit, illa autem voluptas ipsius restinctionis in motu est. Ego quoque, inquit, didicerim libentius si quid attuleris, quam te reprehenderim. Ita ne hoc quidem modo paria peccata sunt. Hoc dictum in una re latissime patet, ut in omnibus factis re, non teste moveamur. An, partus ancillae sitne in fructu habendus, disseretur inter principes civitatis, P. Et hi quidem ita non sola virtute finem bonorum contineri putant, ut rebus tamen omnibus virtutem anteponant; In motu et in statu corporis nihil inest, quod animadvertendum esse ipsa natura iudicet? Nec enim haec movere potest appetitum animi, nec ullum habet ictum, quo pellat animum, status hic non dolendi, itaque in hoc eodem peccat Hieronymus. Itaque multi, cum in potestate essent hostium aut tyrannorum, multi in custodia, multi in exillo dolorem suum doctrinae studiis levaverunt. Simus igitur contenti his.
+
+        Hoc sic expositum dissimile est superiori. Haec et tu ita posuisti, et verba vestra sunt. Primum cur ista res digna odio est, nisi quod est turpis? Quod enim ne vivus quidem, inquit, diutius sentire poterat, quam dum fruebatur, quo modo id potuit mortuo permanere? Minime id quidem, inquam, alienum, multumque ad ea, quae quaerimus, explicatio tua ista profecerit. Semovenda est igitur voluptas, non solum ut recta sequamini, sed etiam ut loqui deceat frugaliter. Sic exclusis sententiis reliquorum cum praeterea nulla esse possit, haec antiquorum valeat necesse est. Graccho, eius fere, aequalí? An dubium est, quin virtus ita maximam partem optineat in rebus humanis, ut reliquas obruat? Praeclare enim Plato: Beatum, cui etiam in senectute contigerit, ut sapientiam verasque opiniones assequi possit.
+
+        Videsne, ut haec concinant? Cuius etiam illi hortuli propinqui non memoriam solum mihi afferunt, sed ipsum videntur in conspectu meo ponere. Huic mori optimum esse propter desperationem sapientiae, illi propter spem vivere. Hoc est dicere: Non reprehenderem asotos, si non essent asoti. Atqui haec patefactio quasi rerum opertarum, cum quid quidque sit aperitur, definitio est. Num igitur dubium est, quin, si in re ipsa nihil peccatur a superioribus, verbis illi commodius utantur? Nam si dicent ab illis has res esse tractatas, ne ipsos quidem Graecos est cur tam multos legant, quam legendi sunt. Cum ageremus, inquit, vitae beatum et eundem supremum diem, scribebamus haec.
+
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;

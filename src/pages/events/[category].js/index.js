6ceb9dfa3f24d6517a40bad139d45f7c41242e06
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-30 16:23:13 -0500

    category dynamic page

---
+++ src/pages/events/[category].js/index.js
@@ -0,0 +1,17 @@
+const EventPage = () => {
+    return (
+        <div>
+            <h1>Events in London</h1>
+            <a href="/event/event1">Event1</a>
+            <a href="/event/event1">Event1</a>
+            <a href="/event/event1">Event1</a>
+            <a href="/event/event1">Event1</a>
+            <a href="/event/event1">Event1</a>
+            <a href="/event/event1">Event1</a>
+            <a href="/event/event1">Event1</a>
+            <a href="/event/event1">Event1</a>
+        </div>
+    );
+};
+
+export default EventPage;

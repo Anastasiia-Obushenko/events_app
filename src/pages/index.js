6a2f9a7d070629570f1f50cf16957b2726f628f3
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-30 14:41:33 -0500

    header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -15,8 +15,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        
+        <header>
+          <nav>
+            <img />
+            <a href='/'>Home</a>
+            <a href='/events'>Events</a>
+            <a href='/about-us'>About Us</a>
+          </nav>
+        </header>
       </main>
     </>
-  )
+  );
 }

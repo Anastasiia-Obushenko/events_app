9110d54f1a457be2e83ae966f737e41522bc6f84
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-30 15:32:28 -0500

    events page content

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Events = () => {
   return (
-    <div>Events</div>
+    <div>
+      <h1>Events Page</h1>
+      <a>
+        <img />
+        <h2>Events in London</h2>
+      </a>
+      <a>
+        <img />
+        <h2>Events in Berlin</h2>
+      </a>
+      <a>
+        <img />
+        <h2>Events in Paris</h2>
+      </a>
+    </div>
   )
 }
 

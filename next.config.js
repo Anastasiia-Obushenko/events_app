6320c93a9f87e8625a12cb58c3c8622179a75a11
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-02-04 21:12:50 -0500

    config image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify:true,
+  images: {
+    domains: ['s1.ticketm.net'],
+  },
 }
 
 module.exports = nextConfig

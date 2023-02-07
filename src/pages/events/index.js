663ddb42c3a9dbcf7a3d85e4139816f009573a0b
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-02-06 23:50:37 -0500

    data fetching

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
-const EventsPage = () => {
+const EventsPage = ({ events_categories }) => {
   return (
     <div>
       <h1>Events Page</h1>
-      <a>
-        <img />
-        <h2>Events in London</h2>
-      </a>
-      <a>
-        <img />
-        <h2>Events in Berlin</h2>
-      </a>
-      <a>
-        <img />
-        <h2>Events in Paris</h2>
-      </a>
+      {events_categories.map(ev => {
+        <a href={`/events/${ev.id}`} key={ev.id}>
+          <Image src={ev.image} alt={ev.title} width={200} height={100} style={{ height: '100%' }} />
+          <h2>{ev.title}</h2>
+        </a>;
+      })}
+
     </div>
   );
 };
 
 export default EventsPage;
+
+export async function getSaticProps() {
+  const { events_categories } = await import('/data/data.json');
+  console.log(events_categories);
+  return {
+    props: {
+      events_categories: events_categories,
+    },
+  };
+}
